9e8f75994cf3ba2396b24f63ade2a2b5371ea4d9
Patrick S. Vogt <patrick.vogt.pv@gmail.com>
2012-11-24 23:00:52 +0100

    fix update flagging

--- src/ch/bedesign/android/law/access/LawUpdater.java
+++ src/ch/bedesign/android/law/access/LawUpdater.java
@@ -1,6 +1,7 @@
 package ch.bedesign.android.law.access;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 import org.apache.http.client.ClientProtocolException;
 
@@ -10,7 +11,6 @@ import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.util.TimingLogger;
 import ch.bedesign.android.law.access.LawUpdater.LoadResult;
 import ch.bedesign.android.law.db.DB;
 import ch.bedesign.android.law.db.DB.Entries;
@@ -20,7 +20,7 @@ import ch.bedesign.android.law.model.EntriesModel;
 import ch.bedesign.android.law.model.LawModel;
 
 public class LawUpdater extends AsyncTask<Long, Object, LoadResult> {
-
+	private static final SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss.SSS");
 	private static final long UPDATE_INTERVALL_MILLIES = 1000 * 60 * 60 * 24 * 7; // once a week
 
 	//	public interface LawUpdateCallback {
@@ -53,23 +53,19 @@ public class LawUpdater extends AsyncTask<Long, Object, LoadResult> {
 		}
 		for (Long lawId : lawIds) {
 			if (lawId != null) {
-				LawModel law = null;
 				ContentResolver resolver = ctx.getContentResolver();
+				Cursor c = null;
 				try {
-					Cursor c = resolver.query(Laws.CONTENT_URI, Laws.PROJECTION_DEFAULT, DB.SELECTION_BY_ID, new String[] { Long.toString(lawId) },
+					c = resolver.query(Laws.CONTENT_URI, Laws.PROJECTION_DEFAULT, DB.SELECTION_BY_ID, new String[] { Long.toString(lawId) },
 							Laws.SORTORDER_DEFAULT);
 					if (c != null && c.moveToFirst()) {
-						law = new LawModel(c);
-						law.setIsUpdating(System.currentTimeMillis());
-						resolver.update(Laws.CONTENT_URI, law.getValues(), DB.SELECTION_BY_ID, new String[] { Long.toString(lawId) });
-						updateLaw(resolver, law);
+						updateLaw(resolver, c);
 					}
 				} catch (Exception e) {
 					Logger.w("Cannot update law", e);
 				} finally {
-					if (law != null) {
-						law.setIsUpdating(-1);
-						resolver.update(Laws.CONTENT_URI, law.getValues(), DB.SELECTION_BY_ID, new String[] { Long.toString(lawId) });
+					if (c != null && !c.isClosed()) {
+						c.close();
 					}
 				}
 			}
@@ -77,45 +73,8 @@ public class LawUpdater extends AsyncTask<Long, Object, LoadResult> {
 		return new LoadResult(true);
 	}
 
-	private void insertLawText(Parser lawData, ContentResolver resolver, LawModel law) throws ClientProtocolException, IOException {
-		TimingLogger timingLogger = new TimingLogger(Logger.TAG, "insertLawText");
-		long lawId = law.getId();
-		String SrNr = law.getCode();
-		int i = 0;
-		Logger.i("Loading law " + law.getCode());
-		while (i < lawData.data.size()) {
-			long p = insert(resolver, new EntriesModel(lawId, -1, lawData.data.get(i).getShortText(), lawData.data.get(i).getText(), null, 0));
-			Parser lawDataSecondLevel = new Parser(ctx, "http://www.admin.ch/ch/d/sr/" + SrNr + "/" + lawData.data.get(i).getLink());
-			lawDataSecondLevel.parse();
-			timingLogger.addSplit("lawDataSecondLevel");
-			int x = 0;
-
-			while (x < lawDataSecondLevel.data.size()) {
-				if (lawDataSecondLevel.data.get(x).getId() == "ArtikelText") {
-					insert(resolver, new EntriesModel(lawId, p, lawData.data.get(i).getShortText(), lawData.data.get(i).getText(), lawDataSecondLevel.data.get(x).getText(), 0));
-				}
-				{
-					long l2 = insert(resolver,
-							new EntriesModel(lawId, p, lawDataSecondLevel.data.get(x).getShortText(), lawDataSecondLevel.data.get(x).getShortText(), null, 1));
-					Parser lawDataThirdLevel = new Parser(ctx, "http://www.admin.ch/ch/d/sr/" + SrNr + "/" + lawDataSecondLevel.data.get(x).getLink());
-					lawDataThirdLevel.parse();
-					timingLogger.addSplit("lawDataSecondLevel");
-					int y = 0;
-					while (y < lawDataThirdLevel.data.size()) {
-						insert(resolver, new EntriesModel(lawId, l2, lawDataSecondLevel.data.get(x).getShortText(), lawDataSecondLevel.data.get(x).getShortText(),
-								lawDataThirdLevel.data.get(y).getText(), 0));
-						y++;
-					}
-				}
-				x++;
-			}
-			i++;
-		}
-		timingLogger.dumpToLog();
-	}
-
-	private void updateLaw(ContentResolver resolver, LawModel law) throws ClientProtocolException, IOException {
-
+	private void updateLaw(ContentResolver resolver, Cursor c) throws ClientProtocolException, IOException {
+		LawModel law = new LawModel(c);
 		Parser lawData = new Parser(ctx, law.getUrl());
 		String lawVersion = lawData.getLawVersion();
 		try {
@@ -130,17 +89,58 @@ public class LawUpdater extends AsyncTask<Long, Object, LoadResult> {
 		} catch (Exception e) {
 			// we do not care
 		}
+		try {
 
-		Logger.i("Parsing law " + law.getName() + " to version " + lawVersion);
-		lawData.parse();
-		resolver.delete(Entries.CONTENT_URI, Entries.SELECTION_LAW, new String[] { Long.toString(law.getId()) });
-		insertLawText(lawData, resolver, law);
+			law.setIsUpdating(System.currentTimeMillis());
+			resolver.update(Laws.CONTENT_URI, law.getValues(), DB.SELECTION_BY_ID, new String[] { Long.toString(law.getId()) });
 
-		Logger.i("Finished parsing law " + law.getName() + " to version " + lawVersion);
-		law.setLastCheck(System.currentTimeMillis());
-		law.setVersion(lawVersion);
+			Logger.i("Parsing law " + law.getName() + " to version " + lawVersion);
+			long start = System.currentTimeMillis();
+			lawData.parse();
+			resolver.delete(Entries.CONTENT_URI, Entries.SELECTION_LAW, new String[] { Long.toString(law.getId()) });
+			insertLawText(lawData, resolver, law);
+
+
+			Logger.i("Finished parsing law " + law.getName() + " to version " + lawVersion + " in " + sdf.format(System.currentTimeMillis() - start));
+			law.setLastCheck(System.currentTimeMillis());
+			law.setVersion(lawVersion);
+		} finally {
+			law.setIsUpdating(-1);
+		}
 		resolver.update(Laws.CONTENT_URI, law.getValues(), Laws.SELECTION_CODE, new String[] { law.getCode() });
+	}
 
+	private void insertLawText(Parser lawData, ContentResolver resolver, LawModel law) throws ClientProtocolException, IOException {
+		long lawId = law.getId();
+		String SrNr = law.getCode();
+		int i = 0;
+		Logger.i("Loading law " + law.getCode());
+		while (i < lawData.data.size()) {
+			long p = insert(resolver, new EntriesModel(lawId, -1, lawData.data.get(i).getShortText(), lawData.data.get(i).getText(), null, 0));
+			Parser lawDataSecondLevel = new Parser(ctx, "http://www.admin.ch/ch/d/sr/" + SrNr + "/" + lawData.data.get(i).getLink());
+			lawDataSecondLevel.parse();
+			int x = 0;
+
+			while (x < lawDataSecondLevel.data.size()) {
+				if (lawDataSecondLevel.data.get(x).getId() == "ArtikelText") {
+					insert(resolver, new EntriesModel(lawId, p, lawData.data.get(i).getShortText(), lawData.data.get(i).getText(), lawDataSecondLevel.data.get(x).getText(), 0));
+				}
+				{
+					long l2 = insert(resolver,
+							new EntriesModel(lawId, p, lawDataSecondLevel.data.get(x).getShortText(), lawDataSecondLevel.data.get(x).getShortText(), null, 1));
+					Parser lawDataThirdLevel = new Parser(ctx, "http://www.admin.ch/ch/d/sr/" + SrNr + "/" + lawDataSecondLevel.data.get(x).getLink());
+					lawDataThirdLevel.parse();
+					int y = 0;
+					while (y < lawDataThirdLevel.data.size()) {
+						insert(resolver, new EntriesModel(lawId, l2, lawDataSecondLevel.data.get(x).getShortText(), lawDataSecondLevel.data.get(x).getShortText(),
+								lawDataThirdLevel.data.get(y).getText(), 0));
+						y++;
+					}
+				}
+				x++;
+			}
+			i++;
+		}
 	}
 
 	private long insert(ContentResolver resolver, EntriesModel entriesModel) {
