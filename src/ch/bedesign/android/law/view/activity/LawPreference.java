4856c06a8000b01cc62a55a2496243643ba56788
Patrick S. Vogt <patrick.vogt.pv@gmail.com>
2012-11-27 21:45:02 +0100

    preferences in debug mode - add delete all

--- src/ch/bedesign/android/law/view/activity/LawPreference.java
+++ src/ch/bedesign/android/law/view/activity/LawPreference.java
@@ -3,9 +3,14 @@ package ch.bedesign.android.law.view.activity;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 import ch.bedesign.android.law.R;
+import ch.bedesign.android.law.db.DB.Entries;
+import ch.bedesign.android.law.db.DB.Laws;
+import ch.bedesign.android.law.db.DbInitaliser;
 import ch.bedesign.android.law.helper.GuiUtils;
+import ch.bedesign.android.law.log.Logger;
 
 public class LawPreference extends PreferenceActivity {
 
@@ -15,6 +20,22 @@ public class LawPreference extends PreferenceActivity {
 		super.onCreate(savedInstanceState);
 
 		addPreferencesFromResource(R.xml.main_law_preference);
+		if (Logger.DEBUG) {
+			addPreferencesFromResource(R.xml.debug_law_preferences);
+			findPreference("prefKeyDeleteAll").setOnPreferenceClickListener(new OnPreferenceClickListener() {
+				
+				public boolean onPreferenceClick(Preference preference) {
+					Logger.w("DELETING ALL");
+					Logger.w("Delete Laws");
+					getContentResolver().delete(Laws.CONTENT_URI, null, null);
+					Logger.w("Delete Entries");
+					getContentResolver().delete(Entries.CONTENT_URI, null, null);
+
+					DbInitaliser.initDb(LawPreference.this);
+					return false;
+				}
+			});
+		}
 
 		findPreference("prefKeyLanguage").setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
 
