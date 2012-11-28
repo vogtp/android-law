692a7da821876a82850877bd6d92d41b9fe7e974
Patrick S. Vogt <patrick.vogt.pv@gmail.com>
2012-11-28 22:43:31 +0100

    install and launch db browser

--- src/ch/bedesign/android/law/view/activity/LawPreference.java
+++ src/ch/bedesign/android/law/view/activity/LawPreference.java
@@ -1,11 +1,24 @@
 package ch.bedesign.android.law.view.activity;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
+import android.widget.Toast;
 import ch.bedesign.android.law.R;
+import ch.bedesign.android.law.db.DB;
 import ch.bedesign.android.law.db.DB.Entries;
 import ch.bedesign.android.law.db.DB.Laws;
 import ch.bedesign.android.law.db.DbInitaliser;
@@ -20,10 +33,11 @@ public class LawPreference extends PreferenceActivity {
 		super.onCreate(savedInstanceState);
 
 		addPreferencesFromResource(R.xml.main_law_preference);
+
 		if (Logger.DEBUG) {
 			addPreferencesFromResource(R.xml.debug_law_preferences);
 			findPreference("prefKeyDeleteAll").setOnPreferenceClickListener(new OnPreferenceClickListener() {
-				
+
 				public boolean onPreferenceClick(Preference preference) {
 					Logger.w("DELETING ALL");
 					Logger.w("Delete Laws");
@@ -32,6 +46,13 @@ public class LawPreference extends PreferenceActivity {
 					getContentResolver().delete(Entries.CONTENT_URI, null, null);
 
 					DbInitaliser.initDb(LawPreference.this);
+					return false;
+				}
+			});
+			findPreference("prefKeyShowDB").setOnPreferenceClickListener(new OnPreferenceClickListener() {
+
+				public boolean onPreferenceClick(Preference preference) {
+					openDbBrowser();
 					return false;
 				}
 			});
@@ -49,4 +70,54 @@ public class LawPreference extends PreferenceActivity {
 		});
 	}
 
+	private void openDbBrowser() {
+		try {
+			installAppIfNotInstalled("oliver.ehrenmueller.dbadmin");
+			String path = (new DB.OpenHelper(getApplicationContext())).getReadableDatabase().getPath();
+			File f = new File(path);
+			File d = new File(Environment.getExternalStorageDirectory() + "/" + DB.DATABASE_NAME);
+			try {
+				copyFile(f, d);
+				Intent i = new Intent(Intent.ACTION_EDIT);
+				i.setData(Uri.parse("sqlite:" + d.getAbsolutePath()));
+				startActivity(i);
+			} catch (IOException e) {
+				Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+			}
+		} catch (Exception e1) {
+		}
+	}
+
+	void copyFile(File src, File dst) throws IOException {
+		FileInputStream in = new FileInputStream(src);
+		FileOutputStream out = new FileOutputStream(dst);
+		FileChannel inChannel = in.getChannel();
+		FileChannel outChannel = out.getChannel();
+		try {
+			inChannel.transferTo(0, inChannel.size(), outChannel);
+		} finally {
+			if (inChannel != null)
+				inChannel.close();
+			if (outChannel != null)
+				outChannel.close();
+			if (in != null)
+				in.close();
+			if (out != null)
+				out.close();
+		}
+	}
+
+	private void installAppIfNotInstalled(String packageName) throws Exception {
+		try {
+			ApplicationInfo info = getPackageManager().
+					getApplicationInfo(packageName, 0);
+			return;
+		} catch (PackageManager.NameNotFoundException e) {
+			Intent i = new Intent(Intent.ACTION_VIEW);
+			Uri fromParts = Uri.parse("market://search?q=pname:" + packageName);
+			i.setData(fromParts);
+			startActivity(i);
+			throw new Exception("installing");
+		}
+	}
 }
